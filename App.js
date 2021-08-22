e6ccbb798e57d8a0ef9a275f6aa954877c0af8b4
Gabriel Oliveira <gabrieldanuzoliveira358@gmail.com>
2021-08-22 00:44:10 -0300

    Iniciando

--- App.js
+++ App.js
@@ -1,21 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {SafeAreaView, StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView>
+    <View>
+      <Text>Cara ou Coroa</Text>
       <StatusBar style="auto" />
     </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
 });
